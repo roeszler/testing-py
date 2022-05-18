be633ca14d814aea731884ed1a4156c7703e09d8
Stuart Roeszler <stuart@roeszler.com>
2022-05-18 07:58:53 +0000

    FEAT: add get user data instructions

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    'https://www.googleapis.com/auth/spreadsheets',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive'
     ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
@@ -12,6 +12,34 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('n3orthotics')
 
-sales = SHEET.worksheet('t-sales')
-data = sales.get_all_values()
-print(data)
+# sales = SHEET.worksheet('orders')
+# data = sales.get_all_values()
+# print(data)
+
+# def start_select_option():
+#     """
+#     Start screen prompting user to:
+#     1. Create a new order, or
+#     2. Retrieve an exsisting order with order number
+#     """
+
+
+def get_user_data():
+    """
+    Get User first name, last name and email from user as a string
+    """
+    print('Where prompted below, please enter your name and email.')
+    print('This user data should be in a valid syntax, for example:\n')
+    print('First Name: Bobby\nLast Name: Hunden')
+    print('Email: bobby123@domain.com\n')
+
+
+def main():
+    """
+    Run all program functions
+    """
+    user = get_user_data()
+    print(user)
+# main()
+
+get_user_data()
